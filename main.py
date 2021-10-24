5d3c942f86626a31e8d1d459681c147a07ddf176
TenTibor <tibqor@gmail.com>
2021-10-24 17:26:07 +0200

    Calc position of blank space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from node import Node
 
-input = "((2 8 3)(1 6 4)(7 M 5))"
+input = "((1 2 3)(M 4 6)(7 5 8))"
 
 root = Node(input)
 root.print_state()
--- node.py
+++ node.py
@@ -6,6 +6,7 @@ class Node:
     h = 0
     score = 0
     before_operator = None
+    blank_pos = 0
 
     def __repr__(self):
         return str(self.state)
@@ -17,21 +18,32 @@ class Node:
     def render_map(self):
         temp = self.state.replace("((", "").replace("))", "").split(" ")
         container = []
+        x = 0
+        y = 0
         for value in temp:
             if ")(" in value:
                 tempValue = value.split(")(")
                 container.append(tempValue[0])
                 self.map.append(container[:])  # ":" to fixed multiple copies in map
+                if tempValue[0] == "M":
+                    self.blank_pos = [x, y]
                 container.clear()
+                y += 1
+                x = 0
                 container.append(tempValue[1])
+                if tempValue[1] == "M":
+                    self.blank_pos = [x, y]
             else:
                 container.append(value)
+                if value == "M":
+                    self.blank_pos = [x, y]
+            x += 1
         self.map.append(container)
 
     def print_state(self):
-        print(self.state)
         print(self.map)
+        print("Blank is on postion: " + str(self.blank_pos[0]) + ":" + str(self.blank_pos[1]))
 
-    # def gen_next_nodes(self):
-    #     calculate possibilities
-
+    def gen_next_nodes(self):
+        # calculate possibilities
+        print("ok")
